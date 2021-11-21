f23704f1497f938e12c95e81132088beca6d0bd7
vincent-tr <vincent.tr@gmail.com>
2021-11-21 11:52:14 +0100

    specific impl

---
+++ components/mylife/rgb_light.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "esphome/core/defines.h"
+
+#ifdef USE_MYLIFE
+#ifdef USE_LIGHT
+
+#include "controller.h"
+
+namespace esphome {
+namespace mylife {
+
+class MylifeRgbLight : public MylifeController {
+public:
+  MylifeRgbLight(MylifeClientComponent *client, light::LightState *target);
+  virtual ~MylifeRgbLight() = default;
+
+  virtual const PluginDefinition *get_plugin_metadata() const override;
+  virtual const std::string &get_component_id() const override;
+};
+
+}  // namespace mylife
+}  // namespace esphome
+
+#endif  // USE_LIGHT
+#endif  // USE_MYLIFE
