07c7bf2a65f2cd5ec094b7e5da4cb1b4e5d878f9
vincent-tr <vincent.tr@gmail.com>
2022-04-26 10:39:11 +0200

    fix stuff

--- components/mylife/client.cpp
+++ components/mylife/client.cpp
@@ -451,7 +451,7 @@ bool MylifeClientComponent::publish(const std::string &topic, const std::string 
 
 bool MylifeClientComponent::publish(const std::string &topic, const char *payload, size_t payload_length, uint8_t qos,
                                   bool retain) {
-  return publish({.topic = topic, .payload = payload, .qos = qos, .retain = retain});
+  return publish({.topic = topic, .payload = std::string(payload, payload_length), .qos = qos, .retain = retain});
 }
 
 bool MylifeClientComponent::publish(const mqtt::MQTTMessage &message) {
